12197abc371758cafd7e3c8265bc4ccb86807685
alejandro <alejandro@Parsers-MacBook-Air.local>
2023-03-06 22:55:47 -0500

    test

---
+++ BusinessApp/Scenes/AddProductModule/AddProductView/AddProductController.swift
@@ -0,0 +1,17 @@
+//
+//  AddProductController.swift
+//  BusinessApp
+//
+//  Created by alejandro on 6/03/23.
+//
+
+import UIKit
+
+class AddProductController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.green
+    }
+
+}
